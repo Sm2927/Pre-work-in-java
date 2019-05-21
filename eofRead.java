5f4b9cd6b7a6460fc09b3255213b8f0dc80775c9
Sm2927 <sovnamohanty19@gmail.com>
2019-05-21 21:44:48 +0530

    Update eofRead.java

--- eofRead.java
+++ eofRead.java
@@ -22,3 +22,27 @@ public class Solution {
     }
 }
 
+
+
+//or use scanner
+import java.util.Scanner;
+
+public class Solution 
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		int lines = 1;
+		while (true)
+		{
+			String line = input.nextLine();
+			System.out.println(lines + " " + line);
+			lines++;
+			if (!input.hasNext())
+			{
+				break;
+			}
+		}
+		input.close();
+	}
+}
